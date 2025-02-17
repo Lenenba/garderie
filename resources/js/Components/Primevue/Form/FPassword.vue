327f4b6f502542ed935465ba1dca3b82320ba6a1
Lenenba <bilitikjulesroger@yahoo.fr>
2025-02-17 16:52:39 -0500

    welcome page

---
+++ resources/js/Components/Primevue/Form/FPassword.vue
@@ -0,0 +1,38 @@
+
+<template>
+    <IftaLabel>
+        <Password  :id="id" v-model="model" class="w-full mt-4" :feedback="false" fluid />
+        <label :for="id">{{ label }}</label>
+    </IftaLabel>
+    <template v-if="message">
+        <Message v-for="(error, index) of message" :key="index" severity="error" size="small" variant="simple">{{ error.message }}</Message>
+    </template>
+</template>
+
+<script setup>
+import { onMounted, ref } from 'vue';
+import Password from 'primevue/password';
+import Message from 'primevue/message';
+import IftaLabel from 'primevue/iftalabel';
+
+defineProps({
+    message: {
+        type: String,
+    },
+    id: {
+        type: String,
+        required: true,
+    },
+    label: {
+        type: String,
+        required: true,
+    },
+});
+
+const model = defineModel({
+    type: String,
+    required: true,
+});
+
+const input = ref(null);
+</script>
---
+++ resources/js/Components/Primevue/Form/Flnput.vue
@@ -0,0 +1,36 @@
+
+<template>
+    <IftaLabel>
+        <InputText :id="id" v-model="model" class="w-full"/>
+        <label :for="id">{{ label }}</label>
+    </IftaLabel>
+    <Message v-if="message" severity="error" size="small" variant="simple">{{ message }}</Message>
+</template>
+
+<script setup>
+import { onMounted, ref } from 'vue';
+import InputText from 'primevue/inputtext';
+import Message from 'primevue/message';
+import IftaLabel from 'primevue/iftalabel';
+
+defineProps({
+    message: {
+        type: String,
+    },
+    id: {
+        type: String,
+        required: true,
+    },
+    label: {
+        type: String,
+        required: true,
+    },
+});
+
+const model = defineModel({
+    type: String,
+    required: true,
+});
+
+const input = ref(null);
+</script>
